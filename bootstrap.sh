034e5d4994d5f6bb3cf3614451215e34a5cf970b
Simone Gentili <simone.gentili@neomobile.com>
2015-07-07 16:25:08 +0200

    Sbiellone

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 sudo apt-get -qq update
 
-sudo apt-get install -y apache2
-sudo apt-get install -y vim
-sudo apt-get install -y tmux
-sudo apt-get install -y curl
-sudo apt-get -y install php5-cli php5-dev php5-fpm php5-curl php5-memcache
+sudo apt-get install -y apache2 vim tmux curl php5-cli php5-dev php5-fpm php5-curl php5-memcache git > /dev/null
+
 sudo curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
-sudo apt-get install -y git
 
-curl -sL https://deb.nodesource.com/setup | sudo bash -
+# Software for compile node ...
+sudo apt-get install -y build-essential g++ > /dev/null
 
-sudo apt-get install -y nodejs
-
-curl http://npmjs.org/install.sh | sh
+# Node installation
+nodeJsVersion="0.10.8"
+urlNodeJs="http://nodejs.org/dist/v${nodeJsVersion}/node-v${nodeJsVersion}.tar.gz"
+mkdir -p /root/nodejs-$nodeJsVersion
+cd /root/nodejs-$nodeJsVersion && wget $urlNodeJs \
+    && tar -xzf node-v${nodeJsVersion}.tar.gz \
+    && cd node-v${nodeJsVersion} \
+    && ./configure && make && make install > /dev/null
